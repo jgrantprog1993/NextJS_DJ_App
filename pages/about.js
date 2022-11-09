1cc0fa773f05ed9a50c195c1528fd2f58b652187
jgrantprog1993 <jason.grant@umail.ucc.ie>
2022-11-09 16:19:18 +0000

    Header tags and Layout, Next Link and Router

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
-
+import Layout from "../components/Layout"
 
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title='About DJ Events'>
         <h1>About</h1>
         <p>This is an app to find latest DJ Events</p>
         <p>Ver: 0.0.1 </p>
-    </div>
+
+    </Layout>
   )
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,4 @@
+import Layout from '../../components/Layout'
 
 export default function EventsPage() {
   return (
