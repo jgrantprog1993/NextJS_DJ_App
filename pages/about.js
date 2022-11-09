f0e5b8b791c7f8b5f01fe1ce0c436c2dbdbc165a
jgrantprog1993 <jason.grant@umail.ucc.ie>
2022-11-09 15:00:43 +0000

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+
+
+export default function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>This is an app to find latest DJ Events</p>
+        <p>Ver: 0.0.1 </p>
+    </div>
+  )
+}
